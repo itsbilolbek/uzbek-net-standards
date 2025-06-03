945bf81f45b4853c4c9bae89088d1d3b72a6a295
Normuminov Bilolbek <bilolbeknormuminov@gmail.com>
2025-06-03 19:11:25 +0900

    Add handling of all text files. Add output file option.

--- scripts/apostrophe_normalizer.py
+++ scripts/apostrophe_normalizer.py
@@ -1,39 +1,61 @@
 import polib, re, argparse
 
-def normalize_apostrophes(po_file_path):
-    """
-    Normalize apostrophes in a .po file to use ʻ (U+02BB) for o' and g', as well as ʼ (U+02BC) for "tutuq belgisi".
-    
-    Args:
-        po_file_path (str): The path to the .po file.
-    """
-    # Load the .po file
-    po = polib.pofile(po_file_path)
-    
-    # Define a regex pattern to match different apostrophe characters
-    # apostrophe_pattern = re.compile(r"[’‘`]")
-    
-    
-    # Iterate through each entry in the .po file
-    for entry in po:
-        if entry.msgstr:
-            # Replace all variations of apostrophes with the standard single quote
-            entry.msgstr = re.sub("O'", "Oʻ", entry.msgstr)
-            entry.msgstr = re.sub("o'", "oʻ", entry.msgstr)
-            entry.msgstr = re.sub("G'", "Gʻ", entry.msgstr)
-            entry.msgstr = re.sub("g'", "gʻ", entry.msgstr)
-            entry.msgstr = re.sub("'", "ʼ", entry.msgstr)
-            # xozir bu yerga '' gap sifatida kelsa 2 ta tutuq belgi boʻlib qolmasin
-            # tayyor roʻyhat boʻlganida soʻzlarni search qilib replace qilardik manimcha
-    
-    # Save the changes back to the .po file
-    po.save(po_file_path)
+def normalize_apostrophes(text: str) -> str:
+    text = re.sub("O'", "Oʻ", text)
+    text = re.sub("o'", "oʻ", text)
+    text = re.sub("G'", "Gʻ", text)
+    text = re.sub("g'", "gʻ", text)
+    text = re.sub("'", "ʼ", text)
+    return text
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Normalize apostrophes in a .po file.")
-    parser.add_argument("--po_file", help="Path to the .po file to normalize.", required=True)
+    parser = argparse.ArgumentParser(description="Normalize apostrophes in a file.")
+    parser.add_argument("--file", help="Path to the file to normalize.", required=True)
+    parser.add_argument("--output", help="Path to save the normalized file. If not provided, the input file will be overwritten.", required=False)
     
     args = parser.parse_args()
+
+    if args.file is None:
+        print("Please provide a path to the .po file using --file argument.")
+        exit(1)
+    elif args.file.endswith('.po'):
+        po = polib.pofile(args.file)
     
-    normalize_apostrophes(args.po_file)
-    print(f"Apostrophes in '{args.po_file}' have been normalized.")
+        # Define a regex pattern to match different apostrophe characters
+        # apostrophe_pattern = re.compile(r"[’‘`]")
+        
+        
+        # Iterate through each entry in the .po file
+        for entry in po:
+            if entry.msgstr:
+                # Replace all variations of apostrophes with the standard single quote
+                entry.msgstr = normalize_apostrophes(entry.msgstr)
+                # xozir bu yerga '' gap sifatida kelsa 2 ta tutuq belgi boʻlib qolmasin
+                # tayyor roʻyhat boʻlganida soʻzlarni search qilib replace qilardik manimcha
+        
+        # Save the changes back to the .po file
+        if args.output:
+            po.save(args.output)
+            print(f"Apostrophes in '{args.file}' have been normalized and saved to '{args.output}'.")
+        else:
+            po.save(args.file)
+            print(f"Apostrophes in '{args.file}' have been normalized.")
+    else:
+        # normalize apostrophes for each line of a file
+        with open(args.file, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+        
+        normalized_lines = [normalize_apostrophes(line) for line in lines]
+        
+        if args.output:
+            with open(args.output, 'w', encoding='utf-8') as f:
+                f.writelines(normalized_lines)
+            print(f"Apostrophes in '{args.file}' have been normalized and saved to '{args.output}'.")
+        else:
+            args.output = args.file
+            with open(args.file, 'w', encoding='utf-8') as f:
+                f.writelines(normalized_lines)
+            print(f"Apostrophes in '{args.file}' have been normalized.")
+           
+
+        
